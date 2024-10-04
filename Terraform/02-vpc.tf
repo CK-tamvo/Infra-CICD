dcf3347a01bc3b30a513ba6b9e56a09b7570a5c9
Tam Vo <tam.vo@cloud-kinetics.com>
2024-10-04 11:34:14 +0700

    Adding VPC

--- Terraform/02-vpc.tf
+++ Terraform/02-vpc.tf
@@ -1,18 +1,18 @@
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-#   name = var.vpc_name
-#   cidr = var.vpc_cidr
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-#   azs             = var.vpc_azs
-#   private_subnets = var.vpc_private_subnets
-#   public_subnets  = var.vpc_public_subnets
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
 
-#   enable_nat_gateway = true
-#   enable_vpn_gateway = true
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-#   tags = {
-#     Terraform = "true"
-#     Environment = "dev"
-#   }
-# }
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
