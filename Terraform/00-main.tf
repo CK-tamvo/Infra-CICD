af49d5bc706f4cc87a02ee678512c83189e3877a
Tam Vo <tam.vo@cloud-kinetics.com>
2024-10-04 10:17:57 +0700

    Update Repo

--- Terraform/00-main.tf
+++ Terraform/00-main.tf
@@ -1,13 +1,3 @@
-terraform {  
-  cloud {
-    organization = "learn_pipeline"
-
-    workspaces {
-      name = "Infra-CICD"
-    }
-  }
-}
-
 provider "aws" {
-  region = "eu-west-3"
+  region = "us-east-1"
 }
